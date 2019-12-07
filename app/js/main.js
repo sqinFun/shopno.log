f9e9c87e5cf8be3fd67ad8b3c5af275018760174
sqinFun <denchyg@yandex.ru>
2019-12-07 16:20:52 +0300

    Добавил 4 секции сайта

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,13 @@
 $(function(){
 
-
+    $(".project-gallery__more-text-btn").click(function() {
+        $(".project-gallery__elem.active").removeClass("active")
+        $(this).parent().parent().addClass("active");
+    });
+    
+    $(".project-gallery__close").click(function() {
+        $(this).parent().parent().removeClass("active");
+    });
 
   
 });
